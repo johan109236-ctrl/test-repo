bd2541e50ead86a46d217823b14bf150f8929144
nepalhimaladventures431 <nepalhimaladventures431@gmail.com>
2026-05-16 00:37:44 +0545

    fixed the issues with 8 pages in trekking weren't linked correctly due to less info in the data typescript file

--- src/app/Nepal/Treks/[slug]/page.tsx
+++ src/app/Nepal/Treks/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import { use } from 'react';
 import { notFound } from 'next/navigation';
 import ExpeditionPage from '../../Expeditions/ExpeditionPage';
-import { treks } from '../../Treks/treks-data';
+import { treks } from '../trek.data';
 
 export default function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = use(params);
